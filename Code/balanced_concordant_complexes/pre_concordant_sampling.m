465526b738c02453a574af24c0349ba665681835
ankueken <ankueken@uni-potsdam.de>
2025-02-11 14:18:41 +0100

    upload

---
+++ Code/balanced_concordant_complexes/pre_concordant_sampling.m
@@ -0,0 +1,94 @@
+function [At,CC] = pre_concordant_sampling(model,name,B)
+
+% Input: model  -   model struct
+%        name   -   string specifying model name
+%        B      -   index of balanced complexes
+%
+% Output: At    -   candidate pairs to check for concordance
+%         CC    -   complex x complex matrix indicating relation
+%                   balanced complexes (diagonal entry 1)
+%                   concordant complexes (trivial -1, otherwise 2)
+
+    CC = sparse(zeros(length(model.complexes)));
+    CC(B,B) = 1;
+    
+    %% species degree
+    species_degree_complexes = sum(model.Y~=0,2);
+    species_degree_reactions = sum(model.S~=0,2);
+    
+    % trivially concordant pairs
+    idx = find(species_degree_complexes==2);
+    for i=1:length(idx)
+        tcc = find(model.Y(idx(i),:)~=0);
+        if CC(tcc,tcc)~=1
+            CC(tcc,tcc) = [0 -1;-1 0];
+           % CC(logical(eye(size(CC)))) = 0;
+        end
+    end
+    
+    %% all possible pairs
+    At=nchoosek(1:length(model.complexes),2);
+    
+    CC_temp=CC;
+    CC_temp(B,:)=5;
+    CC_temp(:,B)=5;
+    
+    [checked_rows,checked_cols]=find(triu(CC_temp,1)~=0);
+    At = setdiff(At,[checked_rows checked_cols; checked_cols checked_rows],'rows');
+    clear checked_rows checked_cols
+    counter=0;
+    At_old_size = size(At,1)*2;
+    %%
+    while size(At,1)<At_old_size*0.99 || counter<50
+        At_old_size = size(At,1);
+        sample_f=[];
+        
+        disp(name)
+    
+        options = optimset('linprog');
+        options.Display = 'off';
+    
+        counter = counter+1;
+        disp('start...')
+    
+        At_rows_to_check = unique(At(:,1));
+    
+        % while ~isempty(At_rows_to_check)
+    
+            fprintf('Round %i, done %i, At size %i \n',counter, length(At_rows_to_check),size(At,1))
+    
+            sv= model.lb + (model.ub-model.lb).*rand(length(model.lb), num_samples);
+    
+            for s=1:num_samples
+    
+                c = sparse([zeros(length(model.lb),1); model.ub-model.lb; model.ub-model.lb]);
+                
+                N = sparse([model.S zeros(size(model.S)) zeros(size(model.S));
+                     eye(size(model.S,2)) eye(size(model.S,2)) -eye(size(model.S,2))]);
+    
+                b = sparse([model.b;sv(:,s)]);
+    
+                [X,mini,ExitFlag]=linprog(-c,[],[],N,b,[model.lb; -ones(length(model.lb)*2,1)*1000],[model.ub; ones(length(model.lb)*2,1)*1000],options);
+    
+                if ExitFlag==1
+                    sample_f(:,s) = sparse(X(1:length(model.lb)));
+                end
+            end
+            clear sv N b c X
+	    fprintf('sampling %i done\n',num_samples)
+                   sample=round(model.A*sample_f,9);
+            clear sample_f
+    
+             [m,n]=size(sample);
+                sample_reshaped = reshape(sample',1,n,m); % every row in sample
+                candidates = std((sample+eps)./(sample_reshaped+eps),0,2,'omitnan')./mean((sample+eps)./(sample_reshaped+eps),2,'omitnan');
+            disp(size(candidates))   
+	     CC_temp = sparse(reshape(candidates,m,m)>0.01);
+	     disp(size(CC_temp))
+           clear samples samples_reshaped
+       
+        [checked_rows,checked_cols]=find(triu(CC_temp,1)~=0);
+        At = setdiff(At,[checked_rows checked_cols; checked_cols checked_rows],'rows');
+	    clear checked_rows checked_cols
+    end
+end
---
+++ Code/run_preprocessing.m
@@ -0,0 +1,130 @@
+function run_preprocessing(files,f,type)
+
+% the code runs the preprocessing of models in folder Models/original 
+% and split of reactions into elementary reaction steps under 
+% the assumption of a fixed ordered binding
+%
+% Input: files - result from dir() where model files are located (e.g. files = dir('Model/original/*.xml');)
+%        f     - (integer) index of the model file for which the code should be executed
+%        type  - (string) the splitting can be done in two ways
+%                'fixed' - fixed order binding, only one order of binding is
+%                    considered the order itself is choosen randomly
+%                'random' - all possible orders of binding considered, see
+%                    convenience kinetics
+% Output: updated model file written into Models/models_with_elementary_steps/
+%
+% Requirements: CobraToolbox, R 
+% R will be called by systems('Rscript R_file.r ags')
+% one may has to give the absolute path to R (e.g. pathToR =
+% '"C:\Users\Anika\AppData\Local\Programs\R\R-4.3.0\bin\Rscript.exe"';))
+
+disp(f)
+addpath(genpath('Code/'))
+%%{
+name = files(f).name(1:end-4)
+
+% disp('set cobra path')
+% COBRA_PATH = '/work/ankueken/Git/cobratoolbox/';
+% addpath(genpath(COBRA_PATH));
+
+model = readCbModel(strcat(files(f).folder,'/',files(f).name));
+
+disp('Done read model')
+
+%% clean model
+% we want to use generic bounds and only account for reversibility
+model.lb(model.lb<0) = -1000;
+model.lb(model.lb>0) = 0;
+model.ub(model.ub<0) = 0;
+model.ub(model.ub>0) = 1000;
+
+model.lb(model.c~=0) = 0;
+model.ub(model.c~=0) = 1000;
+
+if ~isfield(model,'csense')
+    model.csense = repmat('E',size(model.mets));
+end
+
+disp('Clean model')
+model=removeRxns(model,model.rxns(find(all(model.S==0))));
+model=removeMetabolites(model,model.mets(find(all(model.S'==0))));
+
+[solo.x,solo.f,solo.stat,solo.output]=linprog(-model.c,model.S(model.csense=='L',:),model.b(model.csense=='L'),model.S(model.csense=='E',:),model.b(model.csense=='E'),model.lb,model.ub);
+
+[mini,maxi] = linprog_FVA(model,0.001);
+thr=1e-9;
+BLK=model.rxns(find(abs(mini)<thr & abs(maxi)<thr));
+
+model=removeRxns(model,BLK);
+
+[sol.x,sol.f,sol.stat,sol.output]=linprog(-model.c,model.S(model.csense=='L',:),model.b(model.csense=='L'),model.S(model.csense=='E',:),model.b(model.csense=='E'),model.lb,model.ub);
+
+if abs(sol.f)<abs(solo.f)*0.5
+    disp('No biomass due to removal of blocked reactions')
+    % save(['Results/Problems/' name '.mat'])
+    return
+end
+
+clear BLK sol solo files
+
+%% split into elementary reactions
+disp('split reactions into elementary reactions')
+if strcmp(type,'fixed') || strcmp(type,'ordered')
+    model_elementary_fixed = split_into_elementary_rxns_v1(model,'fixed');
+elseif strcmp(type,'random')
+    model_elementary_random = split_into_elementary_rxns_v1(model,'random');
+else 
+    warning('model reactions are split assuming ordered binding, to assume random binding change input arguments')
+end
+
+if strcmp(type,'random')
+    [sol.x,sol.f,sol.stat,sol.output]=linprog(-model_elementary_random.c,model_elementary_random.S(model_elementary_random.csense=='L',:),model_elementary_random.b(model_elementary_random.csense=='L'),model_elementary_random.S(model_elementary_random.csense=='E',:),model_elementary_random.b(model_elementary_random.csense=='E'),model_elementary_random.lb,model_elementary_random.ub);
+    
+    model_elementary_random = convertToIrreversible(model_elementary_random);
+    % save file 
+    mkdir('Models\temp\')
+    save(strcat('Models/temp/',name,'_random.mat'),"model_elementary_random")
+    
+    cd Code/preprocessing/
+
+    % pathToR = '"C:\Users\Anika\AppData\Local\Programs\R\R-4.3.0\bin\Rscript.exe"';
+    system(strjoin({'Rscript get_AY_matrix.r',strcat('../../Models/temp/',name,'_random.mat')}));
+    
+    cd ../../Models/temp/
+    % random model
+    A=importdata(strcat(name,'_random.A'));
+    model_elementary_random.A=sparse(A.data);
+    model_elementary_random.complexes=A.textdata(2:end);
+    clear A
+    Y=importdata(strcat(name,'_random.Y'));
+    model_elementary_random.Y=sparse(Y.data);
+    clear Y 
+    cd ../../
+    
+    save(['Models/models_with_elementary_steps/' name '_pre_balanced_random.mat'],'-v7.3')
+else
+    [sol.x,sol.f,sol.stat,sol.output]=linprog(-model_elementary_fixed.c,model_elementary_fixed.S(model_elementary_fixed.csense=='L',:),model_elementary_fixed.b(model_elementary_fixed.csense=='L'),model_elementary_fixed.S(model_elementary_fixed.csense=='E',:),model_elementary_fixed.b(model_elementary_fixed.csense=='E'),model_elementary_fixed.lb,model_elementary_fixed.ub);
+    
+    model_elementary_fixed = convertToIrreversible(model_elementary_fixed);
+    save(strcat('Models/temp/',name,'_fixed.mat'),"model_elementary_fixed")
+    
+    cd Code/preprocessing/
+    % pathToR = '"C:\Users\Anika\AppData\Local\Programs\R\R-4.3.0\bin\Rscript.exe"';
+    system(strjoin({'Rscript get_AY_matrix.r',strcat('../../Models/original/GEM_xml/',name,'_fixed.mat')}));
+    
+    cd ../../Models/temp/
+    % random model
+    A=importdata(strcat(name,'_fixed.A'));
+    model_elementary_fixed.A=sparse(A.data);
+    model_elementary_fixed.complexes=A.textdata(2:end);
+    clear A
+    Y=importdata(strcat(name,'_fixed.Y'));
+    model_elementary_fixed.Y=sparse(Y.data);
+    clear Y 
+    cd ../../
+    
+    save(['Models/models_with_elementary_steps/' name '_pre_balanced_fixed.mat'],'-v7.3')
+end
+system('rm -r Models/temp/')
+
+end
